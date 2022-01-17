ce30218ec0797b698f1aedeab6ed67c771fe3643
Pengfei Liu <59123869+pfliu-nlp@users.noreply.github.com>
2022-01-16 23:23:52 -0500

    Update wikipedia.py

--- scripts/wikipedia/wikipedia.py
+++ scripts/wikipedia/wikipedia.py
@@ -14,7 +14,7 @@ from datalabs.tasks import TextClassification
 from datalabs import StructuredTextData
 from datalabs.operations.featurize.featurizing import featurizing, Featurizing
 #from .featurize import *
-import wikipedia_ops as wikipedia_ops
+import ops
 
 
 _DESCRIPTION = """\
@@ -51,7 +51,7 @@ _TEST_DOWNLOAD_URL = "https://raw.githubusercontent.com/mhjabreel/CharCnn_Keras/
 def get_operations(module_path:str):
     all_operations = {}
     #module = importlib.import_module(module_path)
-    module = wikipedia_ops
+    module = ops
 
     target_class = None
     print(module)
@@ -92,7 +92,7 @@ class Wikipedia(StructuredTextData):
 
     def __repr__(self):
 
-        module_path = "wikipedia_ops"
+        module_path = "ops"
         all_operations = get_operations(module_path)
         # print(all_operations)
 
